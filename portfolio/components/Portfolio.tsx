3cf8e82c6609ed0fce24d70795a552268e69ddeb
uKruel <29132359+uKruel@users.noreply.github.com>
2022-04-17 22:58:53 +0100

    Add new portfolio starter
    idea is to migrate from gatsby

---
+++ portfolio/components/Portfolio.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Portfolio = () => {
+  return (
+    <div className="flex justify-center items-center bg-teal-500">
+      Portfolio
+    </div>
+  );
+};
+
+export default Portfolio;
---
+++ portfolio/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import Portfolio from "../components/Portfolio";
+
+const Home: NextPage = () => {
+  return <Portfolio />;
+};
+
+export default Home;
