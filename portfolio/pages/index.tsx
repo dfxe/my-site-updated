265731a6402d820ab8881059d65d99c17e50b1b3
Dragos X <29132359+uKruel@users.noreply.github.com>
2022-06-22 13:44:46 +0100

    Add *

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Dragos Dev</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" type="image/x-icon" href="./favicon.ico"></link>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
 
       <Portfolio />
