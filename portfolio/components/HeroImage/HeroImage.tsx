67b0b036d63e6bcfdfeb618b31dafa34e56e8da5
Dragos X <29132359+uKruel@users.noreply.github.com>
2022-08-13 17:50:18 +0300

    Fix hero image centering issue

--- portfolio/components/HeroImage/HeroImage.tsx
+++ portfolio/components/HeroImage/HeroImage.tsx
@@ -5,7 +5,7 @@ import comingSoonPhoto from "./../../public/h3.png";
 const HeroImage = () => {
   return (
     //TODO fix blank space background issue
-    <NoPaddingCard additionalStyles="">
+    <NoPaddingCard additionalStyles="flex justify-center items-center">
       <Image
         src={comingSoonPhoto}
         alt="A picture placeholder for a picture of me."
