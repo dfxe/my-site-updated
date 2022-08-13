84ac1e0360a2fb54b1a06472814530cfbdad0736
Dragos X <29132359+uKruel@users.noreply.github.com>
2022-08-13 22:10:07 +0300

    Add locale en-gb

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -2,4 +2,8 @@ const withMDX = require("@next/mdx")();
 module.exports = withMDX({
   reactStrictMode: true,
   pageExtensions: ["tsx", "mdx"],
+  i18n: {
+    locales: ["en-gb"],
+    defaultLocale: "en-gb",
+  },
 });
