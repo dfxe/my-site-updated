561d4203b8805851db765c917b00bbe8dfbc1aaa
Dragos X <29132359+uKruel@users.noreply.github.com>
2022-10-23 20:29:26 +0300

    Add parallax to project card

--- portfolio/components/ShowcaseProjectCard/ShowcaseProjectCard.tsx
+++ portfolio/components/ShowcaseProjectCard/ShowcaseProjectCard.tsx
@@ -2,30 +2,33 @@ import NoPaddingCard from "../CardPrototypes/NoPaddingCard";
 import Image from "next/image";
 import patternbayImage from "../../public/patternbay.png";
 import Link from "next/link";
+import { useParallax } from "react-scroll-parallax";
 
 const ShowcaseProjectCard = () => {
+  const { ref: parallaxRef } = useParallax({ speed: 8 });
   return (
     <NoPaddingCard additionalStyles={"bg-[#f8fafb]"}>
       <Link
         href="https://patternbay.vercel.app/"
         target={"_blank"}
         rel="noopener noreferrer"
-        style={{ zIndex: 9999, cursor: "pointer" }}
+        style={{ zIndex: 9999 }}
       >
         {/** TODO check this ignore Ref issue */}
         {/*@ts-ignore*/}
-
-        <Image
-          src={patternbayImage}
-          alt="The showcase project, patternbay."
-          style={{
-            zIndex: 9999,
-            cursor: "pointer",
-          }}
-          placeholder="blur"
-          priority
-          quality={"95"}
-        />
+        <div ref={parallaxRef}>
+          <Image
+            src={patternbayImage}
+            alt="The showcase project, patternbay."
+            style={{
+              zIndex: 9999,
+              cursor: "pointer",
+            }}
+            placeholder="blur"
+            priority
+            quality={"95"}
+          />
+        </div>
       </Link>
     </NoPaddingCard>
   );
